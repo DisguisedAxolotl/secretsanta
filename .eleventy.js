5e79d6863ade7877a42fe8dde66279603a8bb9ba
DisguisedAxolotl <jasperhalkowich@gmail.com>
2024-11-29 21:22:12 -0800

    set directory structure

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+export default function (eleventyConfig) {
+    // Enable passthrough for CSS files
+    eleventyConfig.addPassthroughCopy("bundle.css");
+  
+    return {
+      dir: {
+        input: "views", // Input directory for source files
+        output: "_site", // Output directory for the generated site
+        includes: "_includes", // Directory for Nunjucks includes
+        layouts: "_layouts", // Directory for Nunjucks layouts
+      },
+      markdownTemplateEngine: "njk", // Use Nunjucks for Markdown files
+      htmlTemplateEngine: "njk", // Use Nunjucks for HTML files
+      dataTemplateEngine: "njk", // Use Nunjucks for data files
+    };
+  }
+  
